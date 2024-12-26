77b33457cd309158733047bf232a1b9977bbcfef
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-12-26 13:46:45 +0500

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen ">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold  text-center">
+        We&apos;re coming soon
+      </h1>
+    </div>
+  );
+}
